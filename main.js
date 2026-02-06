9f50abec287976f0c97ebd216971e9d24333f51d
kushalchoudhary8949 <kushal.choudhary8949@gmail.com>
2026-02-06 16:33:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+const nameElement = document.getElementById("girlName");
+
+// Get name from URL
+const params = new URLSearchParams(window.location.search);
+const girlName = params.get("name");
+
+// If name exists, show it
+if (girlName) {
+    nameElement.innerHTML = girlName;
+} else {
+    nameElement.innerHTML = "oyee sundrii 💖";
+}
+
+
+
+const noBtn = document.getElementById("btn2");
+const yesBtn = document.getElementById("btn1");
+const question = document.getElementById("id2");
+
+let isLocked = false;   // control variable
+
+// NO runs only if not locked
+function moveButton(){
+
+    if (isLocked) return;   // stop moving after YES
+
+    const maxX = window.innerWidth - noBtn.offsetWidth;
+    const maxY = window.innerHeight - noBtn.offsetHeight;
+
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+
+    noBtn.style.position = "fixed";
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+};
+noBtn.addEventListener("mouseenter",moveButton);
+noBtn.addEventListener("touchstart",moveButton);
+
+// When YES is clicked
+yesBtn.addEventListener("click", function () {
+
+    question.innerHTML = "i knew 💖, but sorry i am not intrested ✨";
+
+    isLocked = true;   // stop movement
+
+    // Bring NO back to normal position
+    noBtn.style.display = "none";
+    yesBtn.style.display="none";
+});
